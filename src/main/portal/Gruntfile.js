4652627c6db71453257d286475040f7b566d3b74
Saulius Mačiulis <saulius@idega.com>
2013-04-08 19:15:04 +0300

    Fixed grunt for js minification

--- src/main/portal/Gruntfile.js
+++ src/main/portal/Gruntfile.js
@@ -1,12 +1,18 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
+	/* Precompile HTML templates.*/
     jst: {
       "dist/debug/templates.js": [
         "app/templates/**/*.html"
       ]
     },
 
+
+    /* Use requirejs optimization tool to merge all 
+     * javascript files and CSS into one minified 
+     * file.
+     */
     requirejs: {
     	compile: {
 	    	options: {
@@ -30,6 +36,8 @@ module.exports = function(grunt) {
     	}
     },
 
+
+    /* Join precompiled templates and minified scripts. */
     concat: {
       dist: {
         src: [
@@ -41,23 +49,57 @@ module.exports = function(grunt) {
       }
     },
 
+    
+    /* Minify joint javascript file. */
     uglify: {
-		minify: {
-	    	files: {
-		    	"dist/release/out.js": ["dist/debug/out.js"]
-	    	}
-		}
+        dist: {
+        	options: {
+        		report: 'min',
+        		// Turn off code optimizations because somehow it removes Backbone parse() method 
+        		// and this results in unusable application.
+        		compress: {
+        			sequences     : false,  // join consecutive statemets with the “comma operator”
+        			properties    : false,  // optimize property access: a["foo"] → a.foo
+        			dead_code     : false,  // discard unreachable code
+        			drop_debugger : false,  // discard “debugger” statements
+        			unsafe        : false, // some unsafe optimizations (see below)
+        			conditionals  : false,  // optimize if-s and conditional expressions
+        			comparisons   : false,  // optimize comparisons
+        			evaluate      : false,  // evaluate constant expressions
+        			booleans      : false,  // optimize boolean expressions
+        			loops         : false,  // optimize loops
+        			unused        : false,  // drop unused variables/functions
+        			hoist_funs    : false,  // hoist function declarations
+        			hoist_vars    : false, // hoist variable declarations
+        			if_return     : false,  // optimize if-s followed by return/continue
+        			join_vars     : false,  // join var declarations
+        			cascade       : false,  // try to cascade `right` into `left` in sequences
+        			side_effects  : false,  // drop side-effect-free statements
+        			warnings      : true,  // warn about potentially dangerous optimizations/code
+        			global_defs   : {}     // global definitions
+        		}
+        	},
+        	files: {
+        		'dist/release/out.js' : ['dist/debug/out.js']
+        	}
+        }
     },
-	
+
+    
+    /* Minify joint css file */
 	cssmin: {
 		"dist/release/out.css": ["dist/debug/out.css"]
 	},
 
+
+	/* Clean dist folder */
     clean: ["dist/"],
   });
 
+
   grunt.loadNpmTasks('grunt-contrib');
-  
+
+
   grunt.registerTask("default", [
       "clean",
       "jst", 
