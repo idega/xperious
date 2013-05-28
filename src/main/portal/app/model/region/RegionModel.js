184848130f96e8556909b3a4018852dd17ec78eb
Saulius Mačiulis <saulius@idega.com>
2013-05-28 09:43:25 +0300

    Migrated portal to separate repo

--- src/main/portal/app/model/region/RegionModel.js
+++
@@ -1,9 +0,0 @@
-define([
-	'app'
-], function(app) {
-
-	return Backbone.RelationalModel.extend({
-		idAttribute: 'id'
-	});
-
-});
--- src/main/portal/app/model/terminal/TerminalModel.js
+++
@@ -1,9 +0,0 @@
-define([
-   'app'
-],function(app) {
-
-	return Backbone.Model.extend({
-		idAttribute: 'code'
-	});
-
-});
